de18fe822e8416d690939dba888de834e8f2a30d
Jyri Lindroos <59079476+keudaope@users.noreply.github.com>
2025-09-01 13:43:49 +0300

    viesti

--- hello.js
+++ hello.js
@@ -1,1 +1,3 @@
+// Alla oleva tulostaa konsoliin viestin "Hello World"
 console.log("Hello world");
+console.log("Pieni possu sanoi, " + "Olen iloinen");
---
+++ testi.js
@@ -0,0 +1,4 @@
+function huuda()
+{
+    alert("Huhuu");
+}
