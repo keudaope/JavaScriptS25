ed62cd7f0dfe76454f07e1acc81925bd16a04a9e
Jyri Lindroos <59079476+keudaope@users.noreply.github.com>
2025-09-15 10:56:19 +0300

    if-else tehtäviä

--- Lumoakatemia/If-elsetehtavia.js
+++ Lumoakatemia/If-elsetehtavia.js
@@ -99,3 +99,164 @@ function parillinenPariton(luku)
 parillinenPariton(0);
 parillinenPariton(3);
 parillinenPariton(16);
+
+/* Tehtävä 11 - Voitto / tappio */
+function voittoTappio(myynnit, ostot)
+{
+    if(myynnit - ostot > 0)
+    {
+        console.log(`Teit voittoa ${myynnit - ostot} €`)
+    }
+    else if (myynnit - ostot < 0)
+    {
+        console.log(`Teit tappiota ${myynnit - ostot} €`)
+    }
+    else
+    {
+        console.log(`Et tehnyt tappiota tai voittoa`)
+    }
+}
+voittoTappio(500, 300);
+voittoTappio(300, 500);
+voittoTappio(300, 300);
+
+/* Tehtävä 13 viikonpäivät */
+
+function viikonpaiva(nro)
+{
+    if(nro == 1)
+    {
+        console.log("maanantai");
+    }
+    else if(nro == 2)
+    {
+        console.log("tiistai");
+    }
+    else if(nro == 3)
+    {
+        console.log("keskiviikko");
+    }
+    else if(nro == 4)
+    {
+        console.log("torstai");
+    }
+    else if(nro == 5)
+    {
+        console.log("perjantai");
+    }
+    else if(nro == 6)
+    {
+        console.log("lauantai");
+    }
+    else if(nro == 7)
+    {
+        console.log("sunnuntai");
+    }
+    else
+    {
+        console.log("Et antanut numeroa välillä 1-7")
+    }
+}
+viikonpaiva(4);
+viikonpaiva(7);
+viikonpaiva(0);
+
+/* Tehtävä 14, ovatko luvut samat */
+function samat(nro1, nro2)
+{
+    if(nro1 == nro2)
+    {
+        console.log("Luvut ovat samat")
+    }
+    else{
+        console.log("Luvut eivät ole samat")
+    }
+}
+
+samat(3, 3);
+samat(2, 3);
+
+/* Tehtävä 15 - voiko äänestää? */
+function aanestys(ika)
+{
+    if(ika >= 18)
+    {
+        console.log("Voit äänestää");
+    }
+    else{
+        console.log("Et voi äänestää")
+    }
+}
+
+aanestys(67);
+aanestys(17);
+
+/* Tehtävä 16 - Kuukauden nro */
+function kuukaudenPaivat(kk)
+{
+    if(kk == 1 || kk == 3 || kk == 5 || kk == 7 || kk == 8 || kk == 10 || kk == 12)
+    {
+        console.log("Kuukaudessa on 31 päivää")
+    }
+    else if(kk == 4 || kk == 6 || kk == 9 || kk == 11)
+    {
+        console.log("Kuukaudessa on 30 päivää")
+    }
+    else if(kk == 2)
+    {
+        console.log("Kuukaudessa on 28/29 päivää")
+    }
+    else{
+        console.log("Et antanut oikeaa kk numeroa (1-12)")
+    }
+}
+kuukaudenPaivat(4);
+kuukaudenPaivat(8);
+kuukaudenPaivat(2);
+
+/* Tehtävä 17 - rahan muunto seteleiksi ja kolikoiksi */
+function muunna(rahasumma)
+{
+    var vsataa, ksataa, sata, vkymmenta, kkymmenta, kymmenen, viisi;
+    var keuroa, euro, vsenttia, ksenttia, kysenttia, viisisenttia;
+    vsataa = Math.floor(rahasumma / 500);
+    rahasumma = rahasumma % 500;
+    ksataa = Math.floor(rahasumma / 200);
+    rahasumma = rahasumma % 200;
+    sata = Math.floor(rahasumma / 100);
+    rahasumma = rahasumma % 100;
+    vkymmenta = Math.floor(rahasumma / 50);
+    rahasumma = rahasumma % 50;
+    kkymmenta = Math.floor(rahasumma / 20);
+    rahasumma = rahasumma % 20;
+    kymmenen = Math.floor(rahasumma / 10);
+    rahasumma = rahasumma % 10;
+    viisi = Math.floor(rahasumma / 5);
+    rahasumma = rahasumma % 5;
+    keuroa = Math.floor(rahasumma / 2);
+    rahasumma = rahasumma % 2;
+    euro = Math.floor(rahasumma / 1);
+    rahasumma = rahasumma % 1;
+    vsenttia = Math.floor(rahasumma / .50);
+    rahasumma = rahasumma % .50;
+    ksenttia = Math.floor(rahasumma / .20);
+    rahasumma = rahasumma % .20;
+    kysenttia = Math.floor(rahasumma / .10);
+    rahasumma = rahasumma % .10;
+    viisisenttia = Math.ceil(rahasumma / .05);
+    console.log(`Antamasi summa vastaa \n${vsataa} * 500 €\n${ksataa} * 200 €\n${sata} * 100 €\n${vkymmenta} * 50 €\n${kkymmenta} * 20 €\n${kymmenen} * 10 €\n${viisi} * 5 €\n${keuroa} * 2 €\n${euro} * 1 €\n${vsenttia} * 0.50 €\n${ksenttia} * 0.20 €\n${kysenttia} * 0.10 €\n${viisisenttia} * 0.05 €`);
+}
+muunna(1378.85);
+
+function onkoKolmio(deg1, deg2, deg3)
+{
+    if(deg1 + deg2 + deg3 == 180)
+    {
+        console.log("Kolmio on mahdollista piirtää")
+    }
+    else{
+        console.log("Kolmiota ei ole mahdollista piirtää")
+    }
+}
+onkoKolmio(100, 42, 38);
+onkoKolmio(101, 42, 38)
